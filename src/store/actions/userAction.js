8b1ded2fa57ee146b4a2bc7e51a2597aaacb05b0
yagnesh parekh <yagneshparekh@rocketmail.com>
2020-12-30 09:47:31 -0500

    change to axios signup post method

--- src/store/actions/userAction.js
+++ src/store/actions/userAction.js
@@ -8,7 +8,10 @@ import {clearRentalUser} from "./rentalUsersActions"
 export const signUpUser = (formData)=>{
 	return dispatch => {
 		// /signup
-		axiosInstance.post("/auth/signup",formData)
+		axiosInstance({
+			method:"post",
+			url:   "https://book-share-server-yp.herokuapp.com/api/auth/signup",
+			formData})
 		.then(res =>{
 			getUser(dispatch);
 		} )
