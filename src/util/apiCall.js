da6cf3d9211695eb8519ccc071adc851ebde91b5
yagnesh parekh <yagneshparekh@rocketmail.com>
2020-12-30 00:15:56 -0500

    change api call base url

--- src/util/apiCall.js
+++ src/util/apiCall.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-	baseURL:"http://localhost:8004",
+	baseURL:"https://book-share-server-yp.herokuapp.com/api",
 	withCredentials: true,
 });
 
